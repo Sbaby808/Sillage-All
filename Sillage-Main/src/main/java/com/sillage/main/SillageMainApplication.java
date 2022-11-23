f305c966a3086248b16fa78c5b0dbdc774ad89ef
dingsheng <dingsheng@weimeng-hosp.com>
2022-11-23 17:26:13 +0800

    Iniital commit!

---
+++ Sillage-Main/src/main/java/com/sillage/main/SillageMainApplication.java
@@ -0,0 +1,13 @@
+package com.sillage.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SillageMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SillageMainApplication.class, args);
+    }
+
+}
---
+++ Sillage-Main/src/test/java/com/sillage/main/SillageMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sillage.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SillageMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
